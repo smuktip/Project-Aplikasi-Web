6e21e58e80634402105d894a2e7809fc5e4840a0
smuktip <143049983+smuktip@users.noreply.github.com>
2023-09-21 09:29:55 +0700

    mukti verif

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\Models\Users;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class LoginController extends Controller
 {
@@ -33,8 +38,69 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+
+    public function __construct(){
+        $this->middleware('guest')->except([
+            'logout', 'home'
+        ]);
+        $this->middleware('auth')->only('logout','home');
+        $this->middleware('verified')->only('home');
     }
+
+    public function register(){
+        return view('auth.register');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|string|email:rfc,dns|max:250|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        event(new Registered($user));
+        $credenBals = $request->only('email', 'password');
+        Auth::attempt($credenBals);
+        $request->session()->regenerate();
+        return redirect()->route('verificaBon.notice');
+    }
+
+    public function login() {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Mohon maaf, kode credenBals Anda Bdak cocok dengan kode credenBals Anda yang telah kami simpan.',
+        ])->onlyInput('email');
+    }
+
+    public function home() {
+        return view('home');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+        ->withSuccess('Selamat, Anda telah berhasil melakukan Logout!');
+    }
+       
 }
+
